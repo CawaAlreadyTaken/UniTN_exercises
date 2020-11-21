c71abe7bca558b2bf4ada83f3ca9d12d4a7f8d6d
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2020-11-21 15:05:09 +0100

    Ex 129 is composed of 3 files.

--- 129.crypt_decrypt_functions.cpp
+++ 129.crypt_decrypt_functions.cpp
@@ -10,14 +10,40 @@ void crypt(char parola [], int chiave) {
 	int i;
 	for (i = 0; i < 100 && parola[i]!='\0'; i++) {
 		char newLetter;
-		if (parola[i]-'a'+chiave>25) {
-			newLetter = (parola[i]-'a'+chiave)%26+'a';
-		} else newLetter = parola[i]+chiave;
+		if (parola[i] >= 'a' && parola[i] <= 'z') {
+			if (parola[i]-'a'+chiave>25) {
+				newLetter = (parola[i]-'a'+chiave)%26+'a';
+			} else newLetter = parola[i]+chiave;
+		} else if (parola[i] >= 'A' && parola[i] <= 'Z') {
+			if (parola[i]-'A'+chiave>25) {
+				newLetter = (parola[i]-'A'+chiave)%26+'A';
+			} else newLetter = parola[i]+chiave;
+			
+		} else {
+			newLetter = '?';
+		}
 		output[i] = newLetter;
 	}
-	output[i] = '\n';
+		output[i] = '\n';
 }
 
 void decrypt(char parola [], int chiave) {
-	cout << 1;
+	int i;
+	for (i = 0; i < 100 && parola[i]!='\0'; i++) {
+		char newLetter;
+		if (parola[i] >= 'a' && parola[i] <= 'z') {
+			if (parola[i]-'a'-chiave<0) {
+				newLetter = (parola[i]-'a'-chiave)%26+'a';
+			} else newLetter = parola[i]-chiave;
+		} else if (parola[i] >= 'A' && parola[i] <= 'Z') {
+			if (parola[i]-'A'-chiave>0) {
+				newLetter = (parola[i]-'A'-chiave)%26+'A';
+			} else newLetter = parola[i]-chiave;
+			
+		} else {
+			newLetter = '?';
+		}
+		output[i] = newLetter;
+	}
+		output[i] = '\n';
 }
